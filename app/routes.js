b523effdda8f971a1f7e84ddaa04e11ce0f6166b
benraine <benraine@users.noreply.github.com>
2023-10-04 11:51:37 +0100

    Initial commit
    GitOrigin-RevId: 2edb6443d005fb51c4d5963dbbc80fc9e56801ad

--- app/routes.js
+++ app/routes.js
@@ -1,28 +1,9 @@
+//
+// For guidance on how to create routes see:
+// https://prototype-kit.service.gov.uk/docs/create-routes
+//
+
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
-
-router.post('/status-update-1', function (req, res) {
-
-      res.redirect('/task-management-HAS-v2/tasklist-status-confirmation')
-    
-  })
-
-  router.post('/status-update-2', function (req, res) {
-
-    res.redirect('/task-management-HAS-v2/tasklist-status-confirmation-2')
-  
-})
-
-// // Logging session data 
- 
-// router.use((req, res, next) => { 
-//   const log = { 
-//   method: req.method, 
-//   url: req.originalUrl, 
-//   data: req.session.data 
-//   } 
-//   console.log(JSON.stringify(log, null, 2)) 
- 
-//   next() 
-//   })
+// Add your routes here
